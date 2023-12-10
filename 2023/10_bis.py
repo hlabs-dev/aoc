2b96d12d1e9cdc7d46a5620af09f9c0507fb455e
Harold Labruyere <harold@hlabs.dev>
2023-12-10 18:04:36 +0100

    Day 10 bis part 2 optimisation

--- 2023/10_bis.py
+++ 2023/10_bis.py
@@ -6,6 +6,7 @@ def offset(pos,di): return (pos[0]+dirs[di][0],pos[1]+dirs[di][1])
 dirs = [(1,0),(0,1),(-1,0),(0,-1)]
 letters = "-F7LJ|S."
 maps = [{0,2},{0,1},{1,2},{0,3},{2,3},{1,3},{0,1,2,3},{}]
+parity = [1,0.5,-0.5,-0.5,0.5,0]
 
 data = aocd.get_data(day=10, year=2023).split('\n')
 m,n = len(data), len(data[0])
@@ -25,11 +26,9 @@ for next_s in neighbours[S]:
                        if {next_s, pos} == {offset(S,di) for di in maps[k]})
         break
 
-part2, in_dir = 0, 0 # 0=out, 1=in left or below, 2=in right
-for i in range(m):
-    in_dir_map=[[1,2,1,0,0,0,0,0],[0,1,2,1,0,1,1,1],[0,0,0,0,1,2,2,2]]
-    for j in range(n):
-        if (i,j) in path: in_dir = in_dir_map[in_dir][sparse[(i,j)]]
-        else: part2+= (in_dir>0)
+part2 = 0
+for i,j in product(range(m),range(n)):
+    if (i,j) in path: s += parity[sparse[(i,j)]]
+    else: part2+= int(s)%2
 
 print("Part1:", len(path)//2, "Part2:", part2)
