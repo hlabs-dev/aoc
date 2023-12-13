fb57bdcfed5580efe690f2160961669cb6047a87
Harold Labruyere <harold@hlabs.dev>
2023-12-13 22:27:26 +0100

    Draft 12

--- 2023/12.py
+++ 2023/12.py
@@ -1,26 +1,26 @@
 import time
-start_time = time.time()
-
-import aocd
 from functools import cache
+import aocd
 
 def parseline(line):
     m,l = line.split()
-    l = tuple(map(int,l.split(',')))
-    return(m,l)
+    return(m,tuple(map(int,l.split(','))))
 
 data = list(map(parseline, aocd.get_data(day=12, year=2023).split('\n')))
 
+start_time = time.time()
+
 @cache
-def dp(s,l):
-    ret, delta = 0, len(s)-sum(l)-len(l)+1
-    if delta < 0: return 0
-    if len(l) == 0: return all(c in '?.' for c in s)
-    ret = dp(s[1:], l) if s[0] in '.?' else 0
-    if all(c in '?#' for c in s[:l[0]]) and (len(s) == l[0] or s[l[0]] in '.?'):
-        ret += dp(s[l[0]+1:], l[1:])
+def dp(S,L):
+    s,l = len(S), len(L)
+    if s < sum(L)+l : return 0
+    if l == 0: return all(c in '?.' for c in S)
+    ret = dp(S[1:], L) if S[0] in '.?' else 0
+    if all(c in '?#' for c in S[:L[0]]) and (s<L[0] or S[L[0]] in '.?'):
+        ret += dp(S[L[0]+1:], L[1:])
     return ret
 
-print("Part1: %d Part2: %d" % (sum(dp(s,l) for s,l in data), sum(dp('?'.join([s]*5),l*5) for s,l in data)))
+print("Part1: %d Part2: %d" % (sum(dp(s+".",l) for s,l in data), 
+                               sum(dp((s+'?')*5,l*5)  for s,l in data)))
 
 print("--- %s seconds ---" % (time.time() - start_time))
