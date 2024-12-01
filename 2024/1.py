aa470d06e9eb0190b53419d814de6499f3fdc274
Harold Labruyere <harold@hlabs.dev>
2024-12-01 09:58:40 +0000

    d1 cleanup

--- 2024/1.py
+++ 2024/1.py
@@ -2,7 +2,7 @@ import aocd
 from collections import Counter
 
 data = aocd.get_data(day=1, year=2024)
-l1, l2 = map(sorted,zip(*(list(map(int,line.split())) for line in data.splitlines())))
+l1, l2 = map(sorted,zip(*((map(int,line.split())) for line in data.splitlines())))
 c = Counter(l2)
 
 print('part1:', sum(abs(a-b) for a,b in zip(l1,l2)),
