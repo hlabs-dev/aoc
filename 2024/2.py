c99aaaf2fa2ddc7695c69d0f65799ad2a2e8e040
Harold Labruyere <harold@hlabs.dev>
2024-12-02 07:09:22 +0000

    Day 2 2024

---
+++ 2024/2.py
@@ -0,0 +1,18 @@
+import aocd
+from collections import Counter
+
+data = aocd.get_data(day=2, year=2024)
+T = list(list(map(int,line.split())) for line in data.splitlines())
+
+def isgood(line):
+    cnt = Counter(a-b for a,b in zip(line[:-1],line[1:]))
+    return all(0<k<4 for k in cnt) or all(0<-k<4 for k in cnt)
+
+def ispart(line):
+    if isgood(line): return 1
+    elif any(isgood(line[:i]+line[i+1:]) for i in range(len(line))): return 2
+    return 0
+
+res = Counter(ispart(line) for line in T)
+
+print('part1:', res[1], 'part2:', res[1]+res[2])
--- 2024/all.py
+++ 2024/all.py
@@ -6,7 +6,7 @@ START = time.time_ns()
 
 def devnull(*args): return
 
-for mday in range(1,2):
+for mday in range(1,3):
     start2 = time.time_ns()
     with open("2024/"+str(mday)+".py") as f:
         for _ in range(1):
