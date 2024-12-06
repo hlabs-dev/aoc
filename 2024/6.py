b30763e0ac32f769561d0c444199f609ea226bc1
Harold Labruyere <harold@hlabs.dev>
2024-12-06 09:24:49 +0000

    Day 6 2024

--- 2024/6.py
+++ 2024/6.py
@@ -1,44 +1,61 @@
 import aocd
-input = aocd.get_data(day=4, year=2024).splitlines()
 
-def rotate(input) :
-    return list(map(list, zip(*input[ ::- 1]) ))
+data = aocd.get_data(day=6, year=2024)
 
-def flip(input):
-    return [l[ ::- 1] for l in input]
+data = [[c for j,c in enumerate(line)] for i,line in enumerate(data.splitlines())]
+m, n = len(data), len(data[0])
 
-def dediag(input) :
-    return [[*line[i:], '#', *line[:i]] for i, line in enumerate(input) ]
+def inrange(i,j): return 0<=i<m and 0<=j<n
+def pmap(): print("\n".join("".join(c for c in line) for line in data))
 
-def count_xmas(input) :
-    return sum("".join(row).count("XMAS")+
-               "".join(row).count("SAMX") for row in input)
+di, dj= -1, 0
+pi, pj = next((i,j) for i in range(m) for j in range(n) if data[i][j] == "^")
+data[pi][pj] = "X"
 
-def print_xmas(input) :
-    for l in input:
-        print("".join(l))
-    print()
+fast = {}
 
-tot = 0
-for _ in range(2):
-    tot += count_xmas(input)
-    print(tot)
-    input = rotate(input)
+for i in range(m):
+    lObs, lObs2 = -2, 0
+    for j in range(n):
+        if data[i][j] == "#": lObs = j
+        else: fast[(i,j,0,-1)] = (i,lObs+1,-1,0)
+        if data[i][n-j-1] == "#": lObs2 = n-j-1
+        else: fast[(i,n-j-1,0,1)] = (i,lObs2-1,1,0)
 
+for j in range(n):
+    lObs, lObs2 = -2, n+1
+    for i in range(m):
+        if data[i][j] == "#": lObs = i
+        else: fast[(i,j,-1,0)] = (lObs+1,j,0,1)
+        if data[m-i-1][j] == "#": lObs2 = m-i-1
+        else: fast[(m-i-1,j,1,0)] = (lObs2-1,j,0,-1)
 
-tot += count_xmas(rotate(dediag(input) ))
-print(tot)
-tot += count_xmas(rotate(dediag(flip(input))))
+def solve(pi,pj,di,dj):
+    part1, part2 = 1, 0
+    cache = {(pi,pj,di,dj)}
+    while(inrange(pi+di,pj+dj)):
+        cell = data[pi+di][pj+dj]
+        if cell == ".":
+            part2 += isloop(pi,pj,dj,-di,pi+di,pj+dj,cache)
+            pi, pj, part1 = pi+di, pj+dj, part1+1
+            data[pi][pj] = "X"
+        elif cell in {"#","0"}: di,dj = dj,-di
+        else: pi, pj = pi+di, pj+dj
+        cache.add((pi,pj,di,dj))
+    print("part1:",part1,"part2:", part2)
 
-print(tot)
+def isloop(pi,pj,di,dj,oi,oj,cache):
+    cache2 = set()
+    while inrange(pi,pj):
+        npi,npj,ndi,ndj = fast[pi,pj,di,dj]
+        if ((oi == pi and di == 0 and (npj-oj)*(pj-oj)<0) or
+            (oj == pj and dj == 0 and (npi-oi)*(pi-oi)<0)):
+            pi,pj,di,dj = oi-di, oj-dj, ndi,ndj
+        else: pi,pj,di,dj = npi,npj,ndi,ndj
 
-def count_mas_cross(input) :
-    return sum((c == line[i+2] == "M" and input [j+1] [i+1] == "A" and input [j+2] [i] == input [j+2] [i+2] == "S") for j,
-               line in enumerate(input[ :- 2]) for i, c in enumerate(line[ :- 2]))
+        if (((pi,pj,di,dj) in cache2) or
+            ((pi,pj,di,dj) in cache)): return 1
+        cache2.add((pi,pj,di,dj))
+    return 0
 
-tot = 0
-for _ in range(4):
-    tot += count_mas_cross(input)
-    input = rotate(input)
-
-print(tot)
+solve(pi,pj,di,dj)
--- 2024/all.py
+++ 2024/all.py
@@ -2,15 +2,15 @@ import time
 import aocd
 from itertools import combinations
 def devnull(*args): return
-iter = 1
+iter = 100
 START = time.time_ns()
 
-for _ in range(iter):    
-    for mday in range(1,6):
-        start2 = time.time_ns()
+for mday in range(1,7):
+    start2 = time.time_ns()
+    for _ in range(iter):       
         with open("2024/"+str(mday)+".py") as f:
             for _ in range(1):
                 exec(f.read(),{"print": devnull})
-        print(mday, ":", (time.time_ns()-start2)/1e9)
-    print(">>>", (time.time_ns()-START)/1e9)
+    print(mday, ":", (time.time_ns()-start2)/1e9/iter)
+    #print(">>>", (time.time_ns()-START)/1e9)
 print(">>>", (time.time_ns()-START)/1e9/iter)
