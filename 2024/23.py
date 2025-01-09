0703a78ab33f507bf27a3e4b5b50125a938e8cf3
Harold Labruyere <harold@hlabs.dev>
2025-01-09 10:23:39 +0000

    day 23 hack 2024

--- 2024/23.py
+++ 2024/23.py
@@ -1,6 +1,7 @@
-from heapq import heapify, heappop, heappush
-from collections import defaultdict
 import aocd
+from heapq import heapify, heappop, heappush
+from collections import defaultdict,Counter
+import bisect
 
 data = aocd.get_data(day=23, year=2024)
 
@@ -14,14 +15,14 @@ for line in data.splitlines():
 def part1():
     visited = set()
     ret = 0
-    for k in M:
+    for k in [k for k in M if k[:1] == 't']:
         visited2 = set()
         for k2 in M[k]:
             if k2 not in visited:
                 inter = M[k] & M[k2]
                 for k3 in inter:
                     if k3 not in visited2 and k3 not in visited:
-                        ret += any(v[:1]=='t' for v in [k,k2,k3])
+                        ret += 1
             visited2.add(k2)
         visited.add(k)
     return ret
@@ -34,12 +35,38 @@ def part2():
         s,keys,rem = heappop(heap)
         for nk in rem:
             if all(key in M[nk] for key in keys):
-                nkeys = sorted([nk]+keys)
+                bs = bisect.bisect_right(keys, nk)
+                nkeys = keys[:bs]+[nk]+keys[bs:]
+                if len(nkeys) == 13: return ",".join(nkeys)
                 tkey = tuple(keys)
                 if tkey not in visited:
                     visited.add(tkey)
                     nrem = {k for k in rem if k!=nk and k in M[nk]}
-                    if len(nrem): heappush(heap,(s+1,nkeys,nrem))
-                    else: ret = nkeys
-    return ",".join(ret)
-print("part1:",part1(),"part2:",part2())
+                    if s+1+len(nrem) >= 13: heappush(heap,(s+1,nkeys,nrem))
+
+def part2_hack():
+    visited = set()
+    heap = [(1,[k],M[k]) for k in M]
+    heapify(heap)
+
+    for k in M:
+        visited.add(k)
+        se = M[k] | {k}
+        fail = 0
+        good = 0
+        for nk in M[k]:
+            if nk in visited:
+                fail += 1
+                if fail == 2: break
+                else: continue
+            nse = se & (M[nk] | {nk})
+            if len(nse)>=13:
+                se = nse
+                good += 1
+                visited.add(nk)
+            else:
+                fail += 1
+                if fail == 2: break
+                else: continue
+        if fail < 2: return ",".join(sorted(se))
+print("part1:",part1(),"part2:",part2_hack())
